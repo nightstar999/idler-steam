243ab45f343a512cad4a288903004c7a22232a2c
nightstar999 <149538791+nightstar999@users.noreply.github.com>
2024-02-26 09:56:18 +0800

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ var password2 = process.env.password2;
 var shared_secret2 = process.env.shared2;
 
 var games2 = [730];  // Enter here AppIDs of the needed games
-var status2 = 7;  // 1 - online, 7 - invisible
+var status2 = 1;  // 1 - online, 7 - invisible
 
 
 user2 = new steamUser();
